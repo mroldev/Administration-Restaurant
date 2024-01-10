27ea45cc8f278eb7cd91f70f74490abfa684929c
Melien Pierre <pimerors@gmail.com>
2024-01-10 14:41:16 +0100

    structure application

---
+++ src/controller/TestCarte.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class TestCarte {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/dal/ConnectionProvider.java
@@ -0,0 +1,19 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//Classe utilitaire pour mutualiser la connexion à la base de données
+
+public abstract class ConnectionProvider {
+	public static Connection getConnection() throws DALException {
+		String url = "jdbc:sqlserver://localhost;databasename=COMPOSANTS;trustservercertificate=true";
+		try {
+			return DriverManager.getConnection(url, System.getenv("USER_SQLSERVER"),
+					System.getenv("PASSWORD_SQLSERVER"));
+		} catch (SQLException e) {
+			throw new DALException("Erreur de connexion a la base de donnees", e);
+		}
+	}
+}
