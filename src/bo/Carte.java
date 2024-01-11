62d678b563f6f29627a11b88bf70b984d8994286
Melien Pierre <pimerors@gmail.com>
2024-01-11 11:51:41 +0100

    Selection de toutes les cartes

--- src/bo/Carte.java
+++ src/bo/Carte.java
@@ -4,21 +4,53 @@ public class Carte {
 	private int id;
 	private String nom;
 	private int idRestaurant;
-	
+
 	public Carte() {
-		
+
 	}
+
 	public Carte(String nom, int idRestaurant) {
-		super();
 		this.nom = nom;
 		this.idRestaurant = idRestaurant;
 	}
-	
+
 	public Carte(int id, String nom, int idRestaurant) {
 		this.id = id;
 		this.nom = nom;
 		this.idRestaurant = idRestaurant;
 	}
-	
-	
+
+	public Carte(String nom) {
+		this.nom = nom;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getIdRestaurant() {
+		return idRestaurant;
+	}
+
+	public void setIdRestaurant(int idRestaurant) {
+		this.idRestaurant = idRestaurant;
+	}
+
+	@Override
+	public String toString() {
+		return "Carte [id=" + id + ", nom=" + nom + ", idRestaurant=" + idRestaurant + "]";
+	}
+
 }
--- src/controller/TestCarte.java
+++ src/controller/TestCarte.java
@@ -1,10 +1,32 @@
 package controller;
 
+import java.util.List;
+import bll.BLLException;
+import bll.CarteBLL;
+import bo.Carte;
+
 public class TestCarte {
+	private static CarteBLL bll;
 
 	public static void main(String[] args) {
-		
+		try {
+			bll = new CarteBLL();
+		} catch (BLLException e) {
+			e.printStackTrace();
+		}
+		listerCartes();
 
+	}
+
+	private static void listerCartes() {
+		try {
+			List<Carte> cartes = bll.selectAll();
+			for (Carte current : cartes) {
+				System.out.println("\t" + current.getId() + ". " + current);
+			}
+		} catch (BLLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
